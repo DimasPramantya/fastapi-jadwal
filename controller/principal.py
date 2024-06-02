fd05f7efcbe3a2114cbedc74f3f5fd9dc3726a70
DimasPramantya <igkdimas@gmail.com>
2024-06-02 23:25:47 +0700

    trying to fix redirect uri

--- controller/principal.py
+++ controller/principal.py
@@ -31,8 +31,7 @@ flow = Flow.from_client_config(
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_secret": CLIENT_SECRET,
-            "redirect_uris": [REDIRECT_URI]
+            "client_secret": CLIENT_SECRET
         }
     },
     scopes=SCOPES
