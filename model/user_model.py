7c31ae8caf103b2da6d44f1f8855d95243bbcc82
DimasPramantya <igkdimas@gmail.com>
2024-06-03 01:48:26 +0700

    FIX issue relationship between user and dosen

--- model/user_model.py
+++ model/user_model.py
@@ -21,7 +21,7 @@ class User(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now) 
     
-    dosen: Mapped["Dosen"] = relationship("Dosen", back_populates="users", lazy="selectin")
+    dosen: Mapped["Dosen"] = relationship("Dosen", back_populates="user", lazy="selectin")
  
 from .dosen_model import Dosen
 
